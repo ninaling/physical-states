89cbd1c6aaffd9ef17dd5260d62262d6fc7e990f
Simon Zhou <simon@s-169-232-94-11.resnet.ucla.edu>
2017-05-10 22:58:28 -0700

    Updated files

--- js/graphite.js
+++ js/graphite.js
@@ -1,7 +1,15 @@
+// add to this list as we go
+const COLORS = {
+  Grey: 0x7F7A78
+};
+
 var scene,
   camera,
   renderer;
 
+// declare objects
+var sphere,
+  cylinder;
 
 var WIDTH = window.innerWidth;
 var HEIGHT = window.innerHeight;
@@ -35,11 +43,41 @@ function initRenderer() {
 // }
 
 function addObject() {
+  //one sphere
+  var sphere_geometry = new THREE.SphereGeometry(1, 10, 10, 0, Math.PI * 2, 0, Math.PI * 2);
+  var sphere_material = new THREE.MeshBasicMaterial({
+    color: COLORS.Grey
+  });
 
+  //one cylinder
+  var cylinder_geometry = new THREE.CylinderGeometry(.1, 0.1, 3, 100, false);
+  // var cylinder_material = new THREE.MeshLambertMaterial({
+  //   wireframe: true,
+  //   color: 0xffffff
+  // });
+  var cylinder_material = new THREE.MeshNormalMaterial();
+
+
+  sphere = new THREE.Mesh(sphere_geometry, sphere_material);
+  cylinder = new THREE.Mesh(cylinder_geometry, cylinder_material);
+  cylinder.rotation.x += 5;
+  cylinder.position.set(.2, .3, 2);
+  scene.add(sphere);
+  scene.add(cylinder);
+
+// var geometry = new THREE.SphereGeometry(3, 50, 50, 0, Math.PI * 2, 0, Math.PI * 2);
+// var material = new THREE.MeshNormalMaterial();
+// sphere = new THREE.Mesh(geometry, material);
+// scene.add(sphere);
 }
 
 function render() {
   requestAnimationFrame(render);
   //add rotation functionality here
-  render.render(scene, camera);
+  // sphere.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
 }
+
+init();
+render();
---
+++ js/test.js
@@ -0,0 +1,22 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(50, 500 / 400, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(500, 400);
+document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.SphereGeometry(3, 50, 50, 0, Math.PI * 2, 0, Math.PI * 2);
+var material = new THREE.MeshNormalMaterial();
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 10;
+var render = function() {
+  requestAnimationFrame(render);
+
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+};
+
+render();
